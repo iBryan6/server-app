4eb5f08c4858276adacc2a9eedc6de70bc1f27ef
iBryan6 <dennis_bryan@hotmail.com>
2018-09-10 23:12:32 -0400

    final

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
+<!DOCTYPE html>
 <?php
-require_once("connectDB.php");
-$db = new DB("127.0.0.1", "serverdb", "root", "");
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        echo json_encode($db->query("SELECT * FROM paquete"));
-        http_response_code(200);
-} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        echo "POST";
-} else {
-        http_response_code(405);
-}
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "serverDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM paquete";
+$result = $conn->query($sql);
 ?>
+
+<h1>SERVIDOR</h1>
+<table>
+        <tr>
+                <th>ID</th>
+                <th>NOMBRE</th>
+                <th>DESCRIPCION</th>
+                <th>PRECIO</th>
+                <th>DURACION</th>
+        </tr>
+<?php
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$row["idPaquete"]."</td>";
+        echo "<td>".$row["nombre"]."</td>";
+        echo "<td>".$row["descripcion"]."</td>";
+        echo "<td>$".$row["costo"]."</td>";
+        echo "<td>".$row["duracion"]." Dias</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+</table>
+</html>
